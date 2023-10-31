dba65af8200c8fc6bc8169ba07cd42de3060a245
aliensbright <144503232+aliensbright@users.noreply.github.com>
2023-10-31 12:21:29 -0700

    ytyg

--- test.py
+++ test.py
@@ -5,14 +5,18 @@ map1=['..........','..........','..........','..........','..........','........
 
 occupied=[[0, 0], [1, 0], [8, 6], [8, 7], [8, 8], [7, 5], [7, 6], [7, 7], [1, 1], [1, 2], [1, 3], [1, 4], [0, 6], [1, 6], [2, 6], [3, 6], [4, 6]]
 
-def Coords(): #Assign Coord Values to variables x and y
+def Coords(i): #Assign Coord Values to variables x and y
     x=(i[m])+1 #X is horizontal from 1-10 left to right indicating which collumn it is in.
     y=(i[m+1])*(-1)+9 #Y is vertical from 0-9 bottom to top indicating which row it is in. 
     return x,y
-def Gameboard(): #Use x and y coords to change a '.' to a 'x'
-    pass
-for i in occupied:
-    Coords()
-print('')
+
+def Gameboard(a,b): #Use x and y coords to change a '.' to a 'x'
+    n =
+    return n
+def main():
+    for coordinates in occupied:
+        Coords(coordinates)
+        Gameboard(x,y)
 for a in range(10):
     print(map1[a])
+main()
